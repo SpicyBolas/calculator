efacb2154311e1d97900a42b186e5fbd5449c99b
larkinaa <104767715+SpicyBolas@users.noreply.github.com>
2023-12-28 22:18:17 +1100

    Added basic button press and equals functionality

--- src/App.js
+++ src/App.js
@@ -9,12 +9,65 @@ import {
 
 
 
-
 function App() {
   const instDisplay = useSelector((state) => state.calculator.instDisplay);
   const display = useSelector((state) => state.calculator.display);
+  const dispatch = useDispatch();
+
+  function handleOnClick(e) {
+    if(e.target.id=='zero'){
+      console.log(e.target.querySelector('span').innerHTML);
+      dispatch(buttonPress(e.target.querySelector('span').innerHTML.toString()));
+      return;
+    }
+
+    dispatch(buttonPress(e.target.innerHTML.toString()));
   
+  }
+
+  function handleClear(e){
+    dispatch(clear());
+  }
+
+  function handleEquals(e){
+    let regex_nums = /([0-9]|\.)+/g;
+    let regex_ops = /[^0-9.]/g;
+    let numbers = display.match(regex_nums);
+    let ops = display.match(regex_ops);
+    
+    let result = Number(numbers[0]);
+    //Modifty for BIDMAS logic
+    //Append the result such that it fits on the screen
+    //Implement logic such that certain operations do nothing sequentially
+    //Update buttons function so that numbers are retained until operation
+    
+    for(let i=1;i<numbers.length;i++){
+      if(ops[i-1]=='+'){
+        result += Number(numbers[i]);
+      }
+      else if(ops[i-1]=='-'){
+        result -= Number(numbers[i]);
+      }
+      else if(ops[i-1]=='x'){
+        result *= Number(numbers[i]);
+      }
+      else if (ops[i-1]=='÷'){
+        result /= Number(numbers[i]);
+      }
+      else{
+        console.log('Error');
+      }
+    }
+    
+    let newDisplay = display + '=' + result.toString();
+    let payload = {display: newDisplay,result: result};
+
+    dispatch(equals(payload));
+
+  }
   
+
+
   return (
     <div className="App">
       <div id='calculator'>
@@ -28,56 +81,56 @@ function App() {
         </div>
 
         <div id='buttons'>
-          <div class='button' id='clear'>
-            <span class='long-butt'>AC</span>
+          <div className='button' id='clear' onClick={handleClear}>
+            <span className='long-butt'>AC</span>
           </div>
-          <div class='button operation' id='divide'>
+          <div className='button operation' id='divide' onClick={handleOnClick}>
             &#247;
           </div>
-          <div class='button' id='seven'>
+          <div className='button' id='seven' onClick={handleOnClick}>
             7
           </div>
-          <div class='button' id='eight'>
+          <div className='button' id='eight' onClick={handleOnClick}>
             8
           </div>
-          <div class='button' id='nine'>
+          <div className='button' id='nine' onClick={handleOnClick}>
             9
           </div>
         
-          <div class='button operation' id='multiply'>
+          <div className='button operation' id='multiply' onClick={handleOnClick}>
             x
           </div>
-          <div class='button' id='four'>
+          <div className='button' id='four' onClick={handleOnClick}>
             4
           </div>
-          <div class='button' id='five'>
+          <div className='button' id='five' onClick={handleOnClick}>
             5
           </div>
-          <div class='button' id='six'>
+          <div className='button' id='six' onClick={handleOnClick}>
             6
           </div>
-          <div class='button operation' id='minus'>
+          <div className='button operation' id='minus' onClick={handleOnClick}>
             -
           </div>
-          <div class='button' id='one'>
+          <div className='button' id='one' onClick={handleOnClick}>
             1
           </div>
-          <div class='button' id='two'>
+          <div className='button' id='two' onClick={handleOnClick}>
             2
           </div>
-          <div class='button' id='three'>
+          <div className='button' id='three' onClick={handleOnClick}>
             3
           </div>
-          <div class='button operation' id='plus'>
+          <div className='button operation' id='plus' onClick={handleOnClick}>
             +
           </div>
-          <div class='button' id='zero'>
-            <span class='long-butt'>0</span>
+          <div className='button' id='zero' onClick={handleOnClick}>
+            <span className='long-butt'>0</span>
           </div>
-          <div class='button' id='decimal'>
+          <div className='button' id='decimal' onClick={handleOnClick}>
             .
           </div>
-          <div class='button operation' id='equals'>
+          <div className='button operation' id='equals' onClick={handleEquals}>
             =
           </div>
 
--- src/features/calculator/calculatorSlice.js
+++ src/features/calculator/calculatorSlice.js
@@ -12,8 +12,8 @@ export const calculatorSlice = createSlice({
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     buttonPress: (state,action) => {
-        state.display = action.payload.display;
-        state.instDisplay = action.payload.button;
+        state.display = state.display + action.payload;
+        state.instDisplay = action.payload;
     },
     equals: (state,action) => {
       //Compute the result of the calculation
@@ -33,14 +33,6 @@ export const calculatorSlice = createSlice({
 });
 
 export const { buttonPress, equals, clear } = calculatorSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectDisplay = (state) => state.calculator.display;
-export const selectInstDisplay = (state) => state.calculator.instDisplay;
-export const selectStoredValue = (state) => state.calculator.storedValue;
-
 
 
 export default calculatorSlice.reducer;
